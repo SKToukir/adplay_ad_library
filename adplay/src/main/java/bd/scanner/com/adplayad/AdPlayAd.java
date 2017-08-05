fa2209386bca4aa810d9507b240714c44c2ff391
SKToukir <toukirsk@gmail.com>
2017-08-05 14:55:21 +0600

    banner toast

--- adplay/src/main/java/bd/scanner/com/adplayad/AdPlayAd.java
+++ adplay/src/main/java/bd/scanner/com/adplayad/AdPlayAd.java
@@ -1,6 +1,8 @@
 package bd.scanner.com.adplayad;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by toukir on 8/5/17.
@@ -11,6 +13,11 @@ public class AdPlayAd {
     public static final String TAG = "adplayad";
 
     public static void bannerAd(){
+        Log.d(TAG,"This is a banner ad");
+    }
+
+    public static void bannerAd(Context context){
+        Toast.makeText(context,"Banner ad",Toast.LENGTH_LONG).show();
         Log.d(TAG,"This is a banner ad");
     }
 }
