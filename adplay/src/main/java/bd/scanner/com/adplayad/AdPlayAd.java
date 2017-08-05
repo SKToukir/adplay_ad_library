354ef7c14c9b32e498a297345a3d13281afd63df
SKToukir <toukirsk@gmail.com>
2017-08-05 14:42:25 +0600

    ad play ad library

---
+++ adplay/src/main/java/bd/scanner/com/adplayad/AdPlayAd.java
@@ -0,0 +1,16 @@
+package bd.scanner.com.adplayad;
+
+import android.util.Log;
+
+/**
+ * Created by toukir on 8/5/17.
+ */
+
+public class AdPlayAd {
+
+    public static final String TAG = "adplayad";
+
+    public static void bannerAd(){
+        Log.d(TAG,"This is a banner ad");
+    }
+}
